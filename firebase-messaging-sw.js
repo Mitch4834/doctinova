e22618f28603e231dae1e0ed87b9d385851a771a
Mitch4834 <42326543+Mitch4834@users.noreply.github.com>
2025-10-28 14:44:15 -0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,10 +1,14 @@
 
 javascript
-importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging.js');
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  messagingSenderId: "327373809245"
+  apiKey: "AIzaSyDsDB8jt6lrrKOA487YDA3ksmGUFd7wExE",
+  authDomain: "doctinova-97d7d.firebaseapp.com",
+  projectId: "doctinova-97d7d",
+  messagingSenderId: "327373809245",
+  appId: "1:327373809245:web:f0958c1bbd5c5c32d34fb4"
 });
 
 const messaging = firebase.messaging();
