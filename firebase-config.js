3b0e1a102734a97eaebfc466f7b387723d36beae
Mitch4834 <42326543+Mitch4834@users.noreply.github.com>
2025-10-28 14:37:38 -0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Remplace avec ta propre config Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDsDB8jt6lrrKOA487YDA3ksmGUFd7wExE",
+  authDomain: "doctinova-97d7d.firebaseapp.com",
+  projectId: "doctinova-97d7d",
+  messagingSenderId: "327373809245",
+  appId:"1:327373809245:web:f0958c1bbd5c5c32d34fb4"
+  measurementId: "G-XX1YLDHRSX"
+};
+
+firebase.initializeApp(firebaseConfig);
+
