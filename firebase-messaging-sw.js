10ea41f3588603d02cf2ed5c65d65ba8b2f7a823
Mitch4834 <42326543+Mitch4834@users.noreply.github.com>
2025-10-28 13:53:06 -0700

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,10 @@
+
+javascript
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging.js');
+
+firebase.initializeApp({
+  messagingSenderId: "327373809245"
+});
+
+const messaging = firebase.messaging();
